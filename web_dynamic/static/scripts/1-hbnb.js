895a1d2a62712e033a4d81c243ff2712e91ecfab
Ajayi Davies <ajayidavies7@gmail.com>
2022-11-28 00:44:26 +0100

    edited flex box for task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,6 @@
 $('document').ready(function () {
-    const amenities = {};
-    $('input[type="checkbox"]').change(function () {
+    let amenities = {};
+    $('INPUT[type="checkbox"]').change(function () {
       if ($(this).is(':checked')) {
         amenities[$(this).attr('data-id')] = $(this).attr('data-name');
       } else {
